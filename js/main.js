b58358a1af5b200ef05fc32df093f0c011f5f56b
SergeyOkladnikov <mr.okladnicoff@yandex.ru>
2022-09-18 16:48:56 +0500

    Добавлены функции в main.js: генерация случайного целого числа в заданном диапазоне, проверка строки на превышение максимальной длины.

--- js/main.js
+++ js/main.js
@@ -1,10 +1,10 @@
 function getRandomInteger(min, max){
-  if (min < max || min == max && Number.isInteger(min)) {
+  if (min < max || min === max && Number.isInteger(min)) {
     min = Math.ceil(min);
     max = Math.floor(max);
     return Math.floor(Math.random() * (max - min + 1)) + min;
   }
-  return 'Wrong range!'
+  return 'Wrong range!';
 }
 
 function checkMaxLength(string, maxLength){
