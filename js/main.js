db3b920658e56c8ef8c7db596bc76bc2c38056c9
SergeyOkladnikov <mr.okladnicoff@yandex.ru>
2022-12-04 21:47:36 +0500

    Создан модуль для работы с сервером

--- js/main.js
+++ js/main.js
@@ -1,7 +1,12 @@
-import { PHOTO_DESCRIPTION_COUNT } from './generation-config.js';
-import { createPhotoDescriptions } from './generation.js';
+import { getData } from './api.js';
+import { renderLoadError } from './thumbnails-render.js';
 import { renderThumbnails } from './thumbnails-render.js';
 import './image-loader.js';
 
-const photoDescriptions = createPhotoDescriptions(PHOTO_DESCRIPTION_COUNT);
-renderThumbnails(photoDescriptions);
+getData((photos) => {
+  renderThumbnails(photos);
+},
+() => {
+  renderLoadError('Не удалось загрузить фотографии(');
+});
+
